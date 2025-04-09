ec212dbaa0d5228e4e8376fcf3c4ab3cbe86e807
jordiindahood <hazemouni123@gmail.com>
2025-04-09 14:46:20 +0100

    script 0

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -5,21 +5,23 @@ script 0
 
 
 def validUTF8(data):
-    bytes_to_process = 0
+    num_bytes = 0
     for byte in data:
-        if bytes_to_process == 0:
+        byte = byte & 0xFF
+
+        if num_bytes == 0:
             if (byte >> 5) == 0b110:
-                bytes_to_process = 1
+                num_bytes = 1
             elif (byte >> 4) == 0b1110:
-                bytes_to_process = 2
+                num_bytes = 2
             elif (byte >> 3) == 0b11110:
-                bytes_to_process = 3
-            elif (byte >> 7) == 0b0:
-                continue  # ASCII
+                num_bytes = 3
+            elif (byte >> 7) == 0:
+                continue
             else:
                 return False
         else:
             if (byte >> 6) != 0b10:
                 return False
-            bytes_to_process -= 1
-    return bytes_to_process == 0
+            num_bytes -= 1
+    return num_bytes == 0
