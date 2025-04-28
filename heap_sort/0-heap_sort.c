b6c13d92347ea6a63c6756d66f41e35f97de6274
jordiindahood <hazemouni123@gmail.com>
2025-04-28 14:50:29 +0100

    heap sort

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -1,4 +1,27 @@
 #include "sort.h"
+
+/**
+* heap_sort - Function to sort the array using the heap sort algorithm.
+* @array: The array to be sorted.
+* @size: The size of the array.
+*/
+void heap_sort(int *array, size_t size)
+{
+	size_t i, temp;
+
+	for (i = size / 2; i > 0; i--)
+	sift_down(array, size, i - 1, size);
+sift_down(array, size, i - 1, size);
+
+for (i = size - 1; i > 0; i--)
+{
+	temp = array[0];
+	array[0] = array[i];
+	array[i] = temp;
+	print_array(array, size);
+	sift_down(array, size, 0, i);
+}
+}
 /**
 * sift_down - Helper function to maintain the heap property.
 * @array: The array to be sorted.
@@ -24,28 +47,5 @@ void sift_down(int *array, size_t size, size_t root, size_t n)
 		array[largest] = temp;
 		print_array(array, size);
 		sift_down(array, size, largest, n);
-	}
-}
-
-/**
-* heap_sort - Function to sort the array using the heap sort algorithm.
-* @array: The array to be sorted.
-* @size: The size of the array.
-*/
-void heap_sort(int *array, size_t size)
-{
-	size_t i, temp;
-
-	for (i = size / 2; i > 0; i--)
-		sift_down(array, size, i - 1, size);
-	sift_down(array, size, i - 1, size);
-
-	for (i = size - 1; i > 0; i--)
-	{
-		temp = array[0];
-		array[0] = array[i];
-		array[i] = temp;
-		print_array(array, size);
-		sift_down(array, size, 0, i);
 	}
 }
--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -6,5 +6,6 @@
 
 void heap_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void sift_down(int *array, size_t size, size_t root, size_t n);
 
 #endif
