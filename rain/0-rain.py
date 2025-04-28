c74f43fc2f3c1bfc15cfb9d1ce5b5e9fe188fa27
jordiindahood <hazemouni123@gmail.com>
2025-04-28 15:04:14 +0100

    rain task

---
+++ rain/0-rain.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def rain(walls):
+    if not walls:  # Return 0 if the list is empty
+        return 0
+
+    n = len(walls)
+    left_max = [0] * n
+    right_max = [0] * n
+    water = 0
+
+    # Fill left_max array
+    left_max[0] = walls[0]
+    for i in range(1, n):
+        left_max[i] = max(left_max[i - 1], walls[i])
+
+    # Fill right_max array
+    right_max[n - 1] = walls[n - 1]
+    for i in range(n - 2, -1, -1):
+        right_max[i] = max(right_max[i + 1], walls[i])
+
+    # Calculate water trapped
+    for i in range(n):
+        water += min(left_max[i], right_max[i]) - walls[i]
+
+    return water
