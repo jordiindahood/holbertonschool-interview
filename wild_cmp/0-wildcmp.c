68724490b58745fc9f49d7bbbaa7af1f7b3a6500
jordiindahood <hazem.ouni123@gmail.com>
2025-11-05 15:47:16 +0100

    script 0

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings
- *           can be considered identical (supports '*')
- * @s1: first string
- * @s2: second string (may contain '*')
+ * wildcmp - Compares two strings and returns 1 if they can be
+ *           considered identical (supports '*' wildcard).
+ * @s1: First string.
+ * @s2: Second string (may contain '*').
  *
- * Return: 1 if identical, otherwise 0
+ * Return: 1 if identical, otherwise 0.
  */
 int wildcmp(char *s1, char *s2)
 {
@@ -15,14 +15,13 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s2 == '*')
 	{
-		/* Skip multiple '*' in a row */
-		while (*(s2 + 1) == '*')
-			s2++;
+		/* Skip multiple '*' recursively */
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
 
-		/* '*' can match empty or any substring */
+		/* '*' can match zero or more characters */
 		if (wildcmp(s1, s2 + 1))
 			return (1);
-
 		if (*s1 && wildcmp(s1 + 1, s2))
 			return (1);
 
