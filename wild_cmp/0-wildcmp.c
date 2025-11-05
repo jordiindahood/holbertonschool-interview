791669fbba7a222c9064c4b5789a4e298c6fe3f7
jordiindahood <hazem.ouni123@gmail.com>
2025-11-05 15:45:15 +0100

    script 0

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -10,27 +10,27 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '\0')
-        return (*s1 == '\0');
+	if (*s2 == '\0')
+		return (*s1 == '\0');
 
-    if (*s2 == '*')
-    {
-        /* Skip multiple '*' in a row */
-        while (*(s2 + 1) == '*')
-            s2++;
+	if (*s2 == '*')
+	{
+		/* Skip multiple '*' in a row */
+		while (*(s2 + 1) == '*')
+			s2++;
 
-        /* '*' can match empty or any substring */
-        if (wildcmp(s1, s2 + 1))
-            return (1);
+		/* '*' can match empty or any substring */
+		if (wildcmp(s1, s2 + 1))
+			return (1);
 
-        if (*s1 && wildcmp(s1 + 1, s2))
-            return (1);
+		if (*s1 && wildcmp(s1 + 1, s2))
+			return (1);
 
-        return (0);
-    }
+		return (0);
+	}
 
-    if (*s1 == *s2)
-        return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 
-    return (0);
+	return (0);
 }
