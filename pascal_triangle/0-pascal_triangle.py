b3454cd1ad6439ba77bbb012983c45d1946fdc57
jordiindahood <hazem.ouni123@gmail.com>
2025-11-11 14:04:07 +0100

    script 0

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -7,7 +7,7 @@ the Pascal’s triangle of n.
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal's triangle of n.
+    Returns a list of lists of integers representing the Pascal's triangle.
     Returns an empty list if n <= 0.
     """
     if n <= 0:
